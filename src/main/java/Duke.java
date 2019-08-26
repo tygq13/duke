b25b9f3032e2e5914b518b7d35ad6745d5497736
tygq13 <yeguoquan13@gmail.com>
2019-08-26 22:06:33 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+	private static void printLine() {
+		System.out.println("---------------------------------------------------");
+	}
+	
+	public static void greet() { 
+		printLine();
+		System.out.println("Hello! I'm Duke");
+		System.out.println("What can I do for you?");
+		printLine();
+	}
+
+	public static void echo(String input) {
+		printLine();
+		System.out.println(input);
+		printLine();
+	}
+
+	public static void exit() {
+		printLine();
+		System.out.println("Bye. Hope to see you again soon!");
+		printLine();
+	}
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +31,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner in = new Scanner(System.in);
+        while (true) {
+        	String input = in.nextLine();
+        	if (input.equals("bye")) {
+        		exit();
+        		break;
+        	} else {
+        		echo(input);
+        	}
+
+        }
     }
 }
